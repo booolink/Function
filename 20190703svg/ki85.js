661f22b5b61b00d47e113b8014b17c78507e5f1d
booolink <aeze716.james@gmail.com>
2019-08-02 18:56:10 +0800

    20190802a

---
+++ 20190703svg/ki85.js
@@ -0,0 +1,91 @@
+
+var place_data = [
+    {
+        tag: "Taipei_City",
+        place: "臺北市"
+    },
+    {
+        tag: "New_Taipei_City",
+        place: "新北市"
+    },
+    {
+        tag: "Taoyuan_City",
+        place: "桃園市"
+    },
+    {
+        tag: "Hsinchu_City",
+        place: "新竹市"
+    },
+    {
+        tag: "Hsinchu_County",
+        place: "新竹縣"
+    },
+    {
+        tag: "Miaoli_County",
+        place: "苗栗縣"
+    },
+    {
+        tag: "Taichung_City",
+        place: "臺中市"
+    },
+    {
+        tag: "Changhua_County",
+        place: "彰化縣"
+    },
+    {
+        tag: "Yunlin_County",
+        place: "雲林縣"
+    },
+    {
+        tag: "Chiayi_County",
+        place: "嘉義縣"
+    },
+    {
+        tag: "Chiayi_City",
+        place: "嘉義市"
+    },
+    {
+        tag: "Tainan_City",
+        place: "臺南市"
+    },
+    {
+        tag: "Kaohsiung_City",
+        place: "高雄市"
+    },
+    {
+        tag: "Pingtung_County",
+        place: "屏東縣"
+    },
+    {
+        tag: "Nantou_County",
+        place: "南投縣"
+    },
+    {
+        tag: "Keelung_City",
+        place: "基隆市"
+    },
+    {
+        tag: "Yilan_County",
+        place: "宜蘭縣"
+    },
+    {
+        tag: "Hualien_County",
+        place: "花蓮縣"
+    },
+    {
+        tag: "Taitung_County",
+        place: "台東縣"
+    },
+    {
+        tag: "Lienchiang_County",
+        place: "連江縣"
+    },
+    {
+        tag: "Kinmen_County",
+        place: "金門縣"
+    },
+    {
+        tag: "Penghu_County",
+        place: "澎湖縣"
+    }
+]
